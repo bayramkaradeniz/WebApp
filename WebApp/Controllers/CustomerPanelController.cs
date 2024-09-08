6ec1097f4f6dba3b4b577cf5b4bcb1789d200805
bayramkaradeniz <141614089+bayramkaradeniz@users.noreply.github.com>
2024-09-08 19:25:17 +0300

    NeOlurNeOlmaz Commiti

--- WebApp/Controllers/CustomerPanelController.cs
+++ WebApp/Controllers/CustomerPanelController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Session;
@@ -7,39 +9,104 @@ using WebApp.Models.Classes;
 
 namespace WebApp.Controllers
 {
-	public class CustomerPanelController : Controller
-	{
-		private Context _context;
+    public class CustomerPanelController : Controller
+    {
+        private Context _context;
 
-      
-		public CustomerPanelController(Context context)
-		{
-			_context = context;
-		}
+
+        public CustomerPanelController(Context context)
+        {
+            _context = context;
+        }
         [Authorize]
         public IActionResult Index()
-		{
-			var username =  HttpContext.Session.GetString("CustomerEmail");
+        {
+            var username = HttpContext.Session.GetString("CustomerEmail");
 
-			var values = _context.Customers.FirstOrDefault(x=>x.CustomerEmail == username);
+            var values = _context.Customers.FirstOrDefault(x => x.CustomerEmail == username);
 
-            ViewBag.U=username;
+            ViewBag.U = username;
             return View(values);
-		}
-        
+        }
+        [HttpPost]
+        public IActionResult UpdateCustomer(Customer customer)
+        {
+            var username = HttpContext.Session.GetString("CustomerEmail");
+
+            var cus = _context.Customers.Where(y => y.CustomerEmail == username).FirstOrDefault();
+
+            if (cus != null)
+            {
+                cus.CustomerName = customer.CustomerName;
+                cus.CustomerSurname = customer.CustomerSurname;
+                cus.CustomerUserName = customer.CustomerUserName;
+                cus.CustomerPassword = customer.CustomerPassword;
+                cus.CustomerEmail = customer.CustomerEmail;
+                cus.CustomerCity = customer.CustomerCity;
+                cus.CustomerPhone = customer.CustomerPhone;
+                cus.CustomerAdress = customer.CustomerAdress;
+                cus.CustomerDistrict = customer.CustomerDistrict;
+                _context.SaveChanges();
+            }
+
+            // Redirect to MyProfile with query parameter to select the "settings" tab
+            return RedirectToAction("MyProfile", new { tab = "settings" });
+        }
+        public IActionResult MyProfile()
+        {
+            var mail = HttpContext.Session.GetString("CustomerEmail");
+
+            var customerId = _context.Customers
+                .Where(c => c.CustomerEmail == mail)
+                .Select(c => c.CustomerId)
+                .FirstOrDefault();
+
+            if (customerId == 0)
+            {
+                // Handle case when no customer is found
+                return NotFound();
+            }
+
+            var customer = _context.Customers.Find(customerId);
+
+            if (customer == null)
+            {
+                // Handle case when customer is not found
+                return NotFound();
+            }
+
+            // Pass the entire customer object to ViewData
+            ViewData["Customer"] = customer;
+
+            var saleTransactions = _context.SaleTransactions
+                .Include(st => st.Customer)
+                .Include(st => st.Product)
+                .Include(st => st.Staff)
+                .Include(st => st.Payment)
+                .Where(st => st.CustomerId == customerId)
+                .ToList();
+
+            return View(saleTransactions);
+        }
+
         public IActionResult MyOrders()
         {
             var username = HttpContext.Session.GetString("CustomerEmail");
 
-            var Id = _context.Customers.Where(y=>y.CustomerEmail == username).Select(x=>x.CustomerId).FirstOrDefault();
+            var Id = _context.Customers.Where(y => y.CustomerEmail == username).Select(x => x.CustomerId).FirstOrDefault();
 
-            var values = _context.SaleTransactions.Include(x=>x.Customer).
-                Include(x=>x.Product)
+            var customer = _context.Customers.Find(Id);
+
+            // Pass the entire customer object to ViewData
+            ViewData["Customer"] = customer;
+
+            var values = _context.SaleTransactions.Include(x => x.Customer).
+                Include(x => x.Product).Include(x => x.Staff).Include(x => x.Payment)
                 .Where(y => y.CustomerId == Id).ToList();
 
             return View(values);
         }
-       
+
         // Gelen mesajlar
         public IActionResult IncomingMessages()
         {
@@ -47,7 +114,7 @@ namespace WebApp.Controllers
 
             var cus = _context.Customers.FirstOrDefault(x => x.CustomerEmail == username);
             // Alıcıya göre mesajları filtreleyin (Receiver)
-            var values = _context.Messages.Where(y => y.Receiver == username).OrderByDescending(x=>x.MessageId).ToList();
+            var values = _context.Messages.Where(y => y.Receiver == username).OrderByDescending(x => x.MessageId).ToList();
 
             // İstatistikleri hesaplayın
             var incoming = _context.Messages.Count(y => y.Receiver == username).ToString();
@@ -57,7 +124,7 @@ namespace WebApp.Controllers
             ViewBag.outgoing = outgoing;
             return View(values);
         }
-      
+
         // Gönderilen mesajlar
         public IActionResult OutgoingMessages()
         {
@@ -79,7 +146,7 @@ namespace WebApp.Controllers
         [HttpGet("/CustomerPanel/MessageDetail/{messageId}")]
         public IActionResult MessageDetail(int messageId)
         {
-          
+
             var values = _context.Messages.FirstOrDefault(x => x.MessageId == messageId);
 
 
@@ -115,22 +182,6 @@ namespace WebApp.Controllers
             return View(values);
         }
 
-        public IActionResult CargoTracking(string c)
-        {
-            var carg = _context.CargoDetails.AsQueryable();
-
-            if (!string.IsNullOrEmpty(c))
-            {
-                carg = carg.Where(y => y.TrackingCode.Contains(c));
-            }
-            return View(carg.ToList());
-        }
-        public IActionResult CargoDetail(string id)
-        {
-            var values = _context.CargoTrackings.Where(x => x.TrackingCode == id).ToList();
-            return View(values);
-        }
-
         [HttpGet]
         public IActionResult NewMessage()
         {
@@ -151,10 +202,67 @@ namespace WebApp.Controllers
 
 
             message.Sender = username;
-            message.Date= DateTime.Parse(DateTime.Now.ToShortDateString());
+            message.Date = DateTime.Parse(DateTime.Now.ToShortDateString());
             _context.Messages.Add(message);
             _context.SaveChanges();
             return RedirectToAction("OutgoingMessages");
+        }
+        public IActionResult CargoTracking(string c)
+        {
+            var carg = _context.CargoDetails.AsQueryable();
+
+            if (!string.IsNullOrEmpty(c))
+            {
+                carg = carg.Where(y => y.TrackingCode.Contains(c));
+            }
+            return View(carg.ToList());
+        }
+        public IActionResult CargoDetail(string id)
+        {
+            var values = _context.CargoTrackings.Where(x => x.TrackingCode == id).ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult NewCargo()
+        {
+            Random rnd = new Random();
+            string[] chars = { "A", "B", "C", "D" };
+            int k1, k2, k3;
+            k1 = rnd.Next(0, 4);
+            k2 = rnd.Next(0, 4);
+            k3 = rnd.Next(0, 4);
+            int s1, s2, s3;
+            s1 = rnd.Next(100, 999);
+            s2 = rnd.Next(10, 99);
+            s3 = rnd.Next(10, 99);
+
+            string code = s1 + chars[k1] + s2 + chars[k2] + s3 + chars[k3];
+            ViewBag.TrackingCode = code;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult NewCargo(CargoDetail cargoDetail)
+        {
+            _context.CargoDetails.Add(cargoDetail);
+            _context.SaveChanges();
+            return RedirectToAction("CargoTracking");
+        }
+        public IActionResult Detail(string id)
+        {
+            var values = _context.CargoTrackings.Where(x => x.TrackingCode == id).ToList();
+            return View(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            // Kullanıcının oturumunu kapat
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            // Session'ı temizle
+            HttpContext.Session.Clear();
+
+            // Giriş sayfasına yönlendir
+            return RedirectToAction("Index", "Login");
         }
     }
 }
